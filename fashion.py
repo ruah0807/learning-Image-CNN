f122b2648351470079cabf85ce1315154438236b
Ruah Kim <kimhk0315@gmail.com>
2024-05-27 15:38:38 +0900

    🎨Using Matplot Library

--- fashion.py
+++ fashion.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 
 
 # GPU 메모리 설정
@@ -17,4 +17,18 @@ if gpus:
 #텐서플로우라이브러리에서 구글이 호스팅해주는 데이터 셋 중하나 
 ( (trainX, trainY), (testX, testY) ) = tf.keras.datasets.fashion_mnist.load_data()
 
-print(trainX)
+print(trainX[0])
+#.shape: 데이터 갯수
+print(trainX.shape)
+
+# 1. 모델만들기
+# 2. compile 하기
+# 3. model.fit 하기 (x와 y데이터를 집어넣어서)
+
+print(trainY)
+
+plt.imshow(trainX[0])
+plt.gray()
+plt.colorbar()
+plt.show()
+# class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankleboot']
