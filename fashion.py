b5c7d140b4e6a8c1515e4535143240fe51225d21
Ruah Kim <kimhk0315@gmail.com>
2024-05-28 22:41:07 +0900

    convolution layer 2

--- fashion.py
+++ fashion.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
-
+import numpy as np
 
 # GPU 메모리 설정
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -17,6 +16,13 @@ if gpus:
 #텐서플로우라이브러리에서 구글이 호스팅해주는 데이터 셋 중하나 
 ( (trainX, trainY), (testX, testY) ) = tf.keras.datasets.fashion_mnist.load_data()
 
+trainX = trainX / 255.0
+textX = testX / 255.0
+
+trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+testX = testX.reshape((testX.shape[0], 28, 28, 1))
+
+
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankleboot']
 
 
@@ -24,11 +30,13 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 
 model = tf.keras.Sequential([
     
     
-    tf.keras.layers.Conv2D(32, (3,3)),
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(28,28,1)),
+    # .MaxPooling2D : 사이즈를 줄이고 중요한 정보를 가운데로 모아주는 함수 
+    tf.keras.layers.MaxPooling2D((2,2)),
     # tf.keras.layers.Dense(128, input_shape=(28,28), activation='relu'),
-    tf.keras.layers.Dense(64, input_shape=(28,28), activation='relu'),
                     # Flatten() : 2D or 3D 데이터를 1차원으로 압축해주는 레이어
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, input_shape=(28,28), activation='relu'),
     tf.keras.layers.Dense(10, input_shape=(28,28), activation='softmax'),
 ])
 # 모델 아웃라인 출력
@@ -38,4 +46,8 @@ model.summary()
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
 
 # 3. model.fit 하기 (x와 y데이터를 집어넣어서)
-model.fit(trainX,trainY, epochs=2)
+model.fit(trainX,trainY, validation_data=(textX,testY), epochs=5)
+
+# 4. 모델이 제대로 잘 실행되었는지 평가
+# score = model.evaluate(testX, testY)
+# print(score)
